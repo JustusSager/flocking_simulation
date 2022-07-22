60af68743d30debde2a9f848f5fe8683b602a918
JustusSager <justus.sager@gmail.com>
2022-07-22 17:49:38 +0200

    Fenstermanagement vereinfachen

--- main.java
+++ main.java
@@ -1,7 +1,18 @@
+import javax.swing.JFrame;
+
 class Main {
     public static void main (String[] args) {
 
-        new MyFrame();
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Flocking Simulation");
+
+        window.add(new MyPanel());
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
 
         System.out.println("Hello world");
     }
